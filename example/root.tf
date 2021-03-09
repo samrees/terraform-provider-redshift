5b770949522fe4e28dab21a86584daa4511eb4fc
Cooper Gillan <cooper.gillan@gmail.com>
2021-03-09 17:02:28 -0600

    Clean up other HCL examples and add comments (#25)
    What
    
    Clean up the HCL in the example/root.tf file and add comments explaining
    what each part does.
    
    Why
    
    This follows the newer Terraform syntax and makes it more clear how to
    use each part of the functionality in the project.
    
    Closes #12

--- example/root.tf
+++ example/root.tf
@@ -1,58 +1,59 @@
-
 variable "url" {
   default = "localhost"
 }
+
 variable "username" {}
+
 variable "password" {}
+
 variable "database_primary" {}
+
 variable "database_test" {
   default = "testdb"
 }
 
+// Set up connection to Redshift
 provider redshift {
-  "url" = "${var.url}",
+  url = "${var.url}",
   user = "${var.username}",
   password = "${var.password}",
   database = "${var.database_primary}"
   sslmode = "disable"
 }
 
+// Create an initial user
 resource "redshift_user" "testuser"{
-  "username" = "testusernew",
-  "password" = "Testpass123"
-  "connection_limit" = "4"
-  "createdb" = true
+  username = "testusernew",
+  password = "Testpass123"
+  connection_limit = "4"
+  createdb = true
 }
 
+// Create another user
 resource "redshift_user" "testuser2"{
-  "username" = "testuser8",
-  "password" = "Testpass123"
-  "connection_limit" = "1"
-  "createdb" = true
+  username = "testuser8",
+  password = "Testpass123"
+  connection_limit = "1"
+  createdb = true
 }
 
-
+// Create a group with the two above-created users
 resource "redshift_group" "testgroup" {
-  "group_name" = "testgroup"
-  "users" = ["${redshift_user.testuser.id}", "${redshift_user.testuser2.id}"]
+  group_name = "testgroup"
+  users = ["${redshift_user.testuser.id}", "${redshift_user.testuser2.id}"]
 }
 
+// Create a `testschemax` schema
 resource "redshift_schema" "testschema" {
-  "schema_name" = "testschemax",
-  "cascade_on_delete" = true
+  schema_name = "testschemax",
+  cascade_on_delete = true
 }
 
+// Add priviliges for the above-created schema
 resource "redshift_group_schema_privilege" "testgroup_testchema_privileges" {
-  "schema_id" = "${redshift_schema.testschema.id}"
-  "group_id" = "${redshift_group.testgroup.id}"
-  "select" = true
-  "insert" = true
-  "update" = true
+  schema_id = "${redshift_schema.testschema.id}"
+  group_id = "${redshift_group.testgroup.id}"
+  select = true
+  insert = true
+  update = true
 }
-
-
-#resource "redshift_database" "testdb" {
-#  "database_name" = "${var.database_test}",
-#  "owner" ="${redshift_user.testuser2.id}",
-#  "connection_limit" = "4"
-#}
